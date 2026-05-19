99b62758ea00de2f3e45cd368324380cf3cf0d1a
ChardonneDixon <chardonne.dixon@gmail.com>
2026-05-19 18:42:02 -0500

    upload the changed ERD

--- scripts/TransientState.js
+++ scripts/TransientState.js
@@ -18,6 +18,27 @@ export const purchaseMineral = () => {
         should use the method of POST, and when you should use PUT.
 
         Only the foolhardy try to solve this problem with code.
+
+        REsolution:
+        export const purchaseMineral = () => {
+    // 1. Get the current colony_id from transient state (via the chosen governor)
+    // 2. Get the current mineral_id from transient state
+
+    // 3. Look at your permanent state (state.purchases)
+    // 4. Try to .find() a purchase object where the colony_id AND mineral_id match your transient state.
+
+    // 5. Did you find one? (Path A)
+        // If yes: We need to use the PUT method.
+        // - Create a new object with the matched purchase's id, colony_id, mineral_id.
+        // - Set its tons to the matched purchase's tons + 1.
+        // - Send a PUT request to update that specific object in the database.
+
+    // 6. Did you NOT find one? (Path B)
+        // If no: We need to use the POST method.
+        // - Create a brand new object with the current colony_id and mineral_id.
+        // - Set its tons to exactly 1.
+        // - Send a POST request to add this new object to the database.
+}
     */
 
 
