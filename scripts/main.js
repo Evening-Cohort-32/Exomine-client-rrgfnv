0a3b7c9b4c0f950a1a160e6d6090f58a9d8ad95d
Stephanie Mahnke <steph.mahnke@gmail.com>
2026-05-21 21:02:48 -0500

    Connected facilityId to mineral inventory

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,22 @@
 import { GovernorSelect } from "./Governors.js";
 import { Colonies } from "./Colonies.js";
 import { facilityOptions } from "./facilitiesChoice.js";
+import { mineralOptions } from "./facilityMinerals.js";
+import { getTransientState } from "./transientState.js"
 
 const renderAllHTML = async () => {
+    const state = getTransientState()
+    
     const governorHTML = await GovernorSelect()
     const coloniesHTML = await Colonies()
     const facilitiesHTML = await facilityOptions()
 
+    const mineralsHTML = await mineralOptions(state.facilityId)
+
     document.querySelector("#facilityOptions").innerHTML = facilitiesHTML
     document.querySelector("#governorSelect").innerHTML = governorHTML
     document.querySelector("#colonyMinerals").innerHTML = coloniesHTML
+    document.querySelector("#facilityMinerals").innerHTML = mineralsHTML
 }
 
 renderAllHTML()
