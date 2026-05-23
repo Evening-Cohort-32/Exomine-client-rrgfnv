5c6f0c7cdc36292ab5459ae36e4c7af69d9eba6f
ChardonneDixon <chardonne.dixon@gmail.com>
2026-05-23 10:42:14 -0500

    updated database and captilization errors

--- scripts/Colonies.js
+++ scripts/Colonies.js
@@ -10,24 +10,39 @@ export const Colonies = async () => {
     }
 
     //Fetch the chosen governor & expand their colony to display name
-    const govResponse = await fetch(`http://localhost:8088/governors/${selectedGovernorId}?_expand=colony`)
+    const govResponse = await fetch(`http://localhost:8088/governors/${selectedGovernorId}`)
     const governor = await govResponse.json()
 
-    let mineralsHTML = `<h2>${governor.colony.name} Minerals </h2>\n`
+    const colonyResponse = await fetch(`http://localhost:8088/colonies/${governor.colonyId}`)
+    const colony = await colonyResponse.json()
+  
+    let mineralsHTML = `<h2>${colony.name} Minerals </h2>\n`
 
-    //Fetch purchases for specified colony and expand the mineral to get its name
-    const purchasesResponse = await fetch(`http://localhost:8088/purchases?colony_id=${governor.colony.id}&_expand=mineral`)
+    const purchasesResponse = await fetch(`http://localhost:8088/purchases?colonyId=${colony.id}`)
     const colonyPurchases = await purchasesResponse.json()
+    
+    const mineralsResponse = await fetch(`http://localhost:8088/minerals`)
+    const minerals = await mineralsResponse.json()
+
+    // Map over the purchases to get the mineral name for each purchase
+    const purchasesWithMinerals = colonyPurchases.map(purchase => {
+        const mineral = minerals.find(mineral => mineral.id === purchase.mineralId)
+        return {
+            ...purchase,
+            mineral
+        }
+    })
+
 
     //Build HTML List
-    if (colonyPurchases.length === 0) {
+    if (purchasesWithMinerals.length === 0) {
         mineralsHTML += `<p>No minerals purchased yet.</p>`
     } else {
         mineralsHTML += `<ul>\n`
         
-        //Map over purchase to creat the ilst items
-        const listItems = colonyPurchases.map(purchase => {
-            return `<li>${purchase.tons} tons of ${purchase.mineral.name}</li>`
+        //Map over purchase to create the list items
+        const listItems = purchasesWithMinerals.map(purchase => {
+             return `<li>${purchase.tons} tons of ${purchase.mineral?.name ?? "Unknown mineral"}</li>`
         })
   
         mineralsHTML += listItems.join("\n")
